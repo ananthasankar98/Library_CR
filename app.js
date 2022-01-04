a9711eeeac3e32ef6c3bc4dcd82a95c8a9287dec
ananthasankar98 <93582748+ananthasankar98@users.noreply.github.com>
2022-01-04 17:40:04 +0530

    Library

--- app.js
+++ app.js
@@ -1,41 +1,72 @@
-const express = require('express');
-const chalk = require('chalk');
-const path = require('path');
-const bodyparser = require('body-parser');  //previously was a comment, changed
+const express = require('express'); 
+const path = require ('path'); 
+const bodyParser = require('body-parser'); //Part #1 Point 2
 const cors = require('cors');
+const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
 
-const nav = [
-    { link: "/books", title: "Books" },
-    { link: "/authors", title: "Authors" },
-    { link: "/books/admin", title: "Add Book" },
+const nav= [
+    {
+        link:"/books", title:"Books"},
+    {
+        link:"/authors",title:"Authors"},
+    {
+        link:"/addbook",title:"Add Book"},
+    {
+        link:"/addauthor",title:"Add Author"},
+]; //Part #2 Point 6
 
-];
-const homeRouter = require('./src/routes/homeroute');(nav); // Type error spelling mistake
 
-const app = new express; //init a representative of express
 
-app.use(cors());
+// const nav = require('./src/data/nav'); //Part #2 Point 6
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homerouter'); //Part #1 Point 3
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+app.use(cors()); //Part #2 Point 7
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-//Setting the path for static files
-app.use(express.static(path.join(__dirname, '/public')));
-//Requesting for /books, use booksRouter
-app.use('/books', booksRouter);
+app.use(express.static(path.join(__dirname , '/public'))); 
 
-//Setting up ejs engine and ejs file path
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
 
-//Routing
-app.get('/', function (req, res) {
-    res.render("index",
-        {
-            nav,
-            title: "Library"
-        });
-})
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter);
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
 
-app.listen(3000, function () {
-    console.log("Listening to port" + chalk.green(3000));
-}); //creating a port
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+
+});       
+    
+
+app.listen(port, host, function(){
+    console.log("Server Ready on 5000");//Part #1 Point 5
+});  
+
+
+
+
+
+
+
+
+
+
+
+
