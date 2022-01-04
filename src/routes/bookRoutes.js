73c1b25c08f23bcf602ff1cad74a3f5a1a47e5b6
ananthasankar98 <93582748+ananthasankar98@users.noreply.github.com>
2022-01-04 18:06:04 +0530

    Library

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,52 +1,117 @@
-const express = require('express');
-const booksRouter = express.Router(); //books router
-const Bookdata = require('../model/BookModel');
+const express = require('express'); 
+const booksRouter = express.Router();
+// const books = require('../data/books');
+const bookdata = require('../model/BookModel');
 
-function router(nav) {
 
-    booksRouter.get('/', function (req, res) {
-        Bookdata.find()
-            .then(function (books) {
-                res.render("books",
-                    {
-                        nav,
-                        title: "Books",
-                        books
-                    });
-            })
+
+//router to render books page
+booksRouter.get('/',function(req,res){
+
+    bookdata.find() 
+    .then(function (books) {
+
+    res.render('books',{
+        books
     });
 
-    booksRouter.get('/admin', function (req, res) {
-        res.render('addbook', {
-            nav,
-            title: "Add Book"
-        })
-    });
+    })
+});
 
-    booksRouter.post('/add', function (req, res) {
-        var item = {
-            title: req.body.title,
-            author: req.body.author,
-            image: req.body.image
+
+
+//router to render addbook page
+booksRouter.get('/addbook',function(req,res){
+    res.render('addbook',{});
+
+});
+
+
+
+
+//router to add book
+booksRouter.post('/add', function (req, res) {
+
+        var item={
+            title:req.body.title,
+            author:req.body.author,
+            image:req.body.image,
+            about:req.body.about
         }
-        const book = new Bookdata(item);
+        console.log(item)  ;
+        const book = new bookdata(item);
         book.save();
         res.redirect('/books');
-    });
 
-    booksRouter.get('/:id', function (req, res) {
-        const ID = req.params.id;
-        Bookdata.findOne({ _id: ID })
+    })
+
+
+
+//router for singlebook
+booksRouter.get('/:id',function(req,res){
+    const id = req.params.id;
+    bookdata.findOne({ _id: id })
             .then(function (book) {
-                res.render("book",
-                    {
-                        nav,
-                        title: "Books",
-                        book
-                    });
-            })
-    });
+                res.render('book', {
+                    book
+                })
 
-    return booksRouter;
-}
-module.exports = users;  //changed to users
+            })
+    
+});
+
+
+
+
+//router to delete book
+booksRouter.post('/delete', function (req, res) {//Part #2 Point 9
+
+    const id = req.body.id;  
+
+    bookdata.deleteOne({ _id: id })
+        .then(function () {
+
+            res.redirect('/books')
+
+        })  
+})
+
+
+
+//router to edit book
+booksRouter.post('/edit', function (req, res) {
+
+    bookdata.findById(req.body.id, function(err, data){
+        if (err) {
+            throw err;
+        }
+        else {
+            res.render('editbook', {data})
+        }
+    })
+})
+
+
+
+//router to update book
+booksRouter.post('/update', function (req, res) {//Part #2 Point 9
+
+    bookdata.findByIdAndUpdate(req.body.id, { $set: req.body }, function (err, data) {
+        if (err) {
+            res.json({ status: "Failed" });
+        }
+        else if (data.n == 0) {
+            res.json({ status: "No match Found" });
+        }
+        else {
+            res.redirect("/books");
+        }
+
+    }) 
+})
+
+
+
+
+
+module.exports = booksRouter;
